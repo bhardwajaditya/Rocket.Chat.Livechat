df5041e62576ef299e172fceaea49bb71bdc0cc7
Aditya Bhardwaj <aditya.1998bhardwaj@gmail.com>
2021-11-02 01:07:05 +0530

    Add signature4 signing for ES requests

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -4,6 +4,22 @@ import { store } from '../store';
 
 const { localStorage } = window;
 
+const urlDecomposition = (url) => {
+	let tempUrl = url.split('://');
+	if (tempUrl.length < 2) {
+		throw new Error('URL is not valid.');
+	}
+	const protocol = tempUrl[0];
+	tempUrl = tempUrl.slice(1).join('://');
+	tempUrl = tempUrl.split('/');
+	const host = tempUrl[0];
+	const path = `/${ tempUrl.slice(1).join('/') }`;
+	tempUrl = path.split('?');
+	const pathname = tempUrl[0];
+	const query = tempUrl.slice(1).join('?');
+
+	return { protocol, host, path, pathname, query };
+};
 class Logger {
 	constructor(name, key = 'logs') {
 		this.name = name;
@@ -35,23 +51,44 @@ class Logger {
 	}
 
 	async sendLogsToES() {
+		const aws4 = require('aws4');
 		const { config: { settings: {
 			livechat_enable_elastic_search_logs: enable,
 			livechat_elastic_search_url: url,
 			livechat_elastic_search_index: index,
-			livechat_elastic_search_token: token,
+			livechat_elastic_search_access_key: access_key,
+			livechat_elastic_search_access_key_secret: secret_access_key,
 		} } } = store.state;
 		if (!enable) {
 			return;
 		}
-		await fetch(`${ url }/${ index }/_bulk`, {
+
+		const uri = `${ url }/${ index }/_bulk`;
+		const { host, pathname } = urlDecomposition(uri);
+		const region = 'us-east-1';
+		const service = 'es';
+		const content_type = 'application/json';
+
+		const opts = {
 			method: 'POST',
+			host,
+			path: pathname,
+			region,
+			service,
 			headers: {
-				'Content-Type': 'application/json',
-				Authorization: `Basic ${ token }`,
+				'Content-Type': content_type,
 			},
 			body: this.activeLogs,
+		};
+
+		aws4.sign(opts, {
+			secretAccessKey: secret_access_key,
+			accessKeyId: access_key,
 		});
+
+		await fetch(uri, opts)
+			.then((response) => console.log(response))
+			.catch((error) => console.log(error));
 		this.clearLogs();
 	}
 }
